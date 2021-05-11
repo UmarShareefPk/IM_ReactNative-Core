b4d28d6dc22ee56d2fc17b3b9b2a3bd8e0726d18
Umar Shareef <Omar.Sharif91@gmail.com>
2021-05-12 02:42:00 +0500

    Update IncidentFields.js

--- Im_React_Native/screens/incident/incidentDetails/IncidentFields.js
+++ Im_React_Native/screens/incident/incidentDetails/IncidentFields.js
@@ -9,26 +9,27 @@ import { setStatusBarBackgroundColor } from 'expo-status-bar';
 const IncidentFields = (props) => {
     return (
       <View>
+      <View style={{flexDirection:'row'}}>
         <View style={styles.fields}>
           <Text style={styles.field}>
             Status: <Text style={styles.fieldValue}>New</Text>
           </Text>
-
           <Text style={styles.field}>
-            Assigned To: <Text style={styles.fieldValue}>Umar Shareef</Text>
+            Due Date: <Text style={styles.fieldValue}>In 2 Days</Text>
           </Text>
         </View>
 
         <View style={styles.fields}>
           <Text style={styles.field}>
-            Due Date: <Text style={styles.fieldValue}>In 2 Days</Text>
+            Assigned To: <Text style={styles.fieldValue}>Umar Shareef</Text>
           </Text>
 
           <Text style={styles.field}>
             Start Date: <Text style={styles.fieldValue}>In 2 Days</Text>
           </Text>
         </View>
-        <View style={{flexDirection:'row', justifyContent:'flex-end'}}>
+</View>
+        <View style={{ flexDirection: "row", justifyContent: "flex-end" }}>
           <Button
             onPress={() => setEditAble(!editAble)}
             title=""
@@ -47,23 +48,23 @@ const IncidentFields = (props) => {
     
     fields:{   
         alignSelf:'stretch',
-        paddingHorizontal:20,
+        paddingHorizontal:31,
         marginVertical:5,
-        flexDirection:'row',
-        justifyContent:'space-between' ,
-        width: Dimensions.get('window').width  
+        flexDirection:'column',
+      //  justifyContent:'space-between' ,
+      //  width: Dimensions.get('window').width  
     },
     field:{
        color:'#1A237E',
         fontSize:12,
-        fontWeight:'bold'
+        //fontWeight:'bold'
     },
     fieldValue:{
         color:'#848B98',
         //cfontWeight:'bold'
     },
     editBtn:{
-      marginEnd:15,
+      marginEnd:10,
       backgroundColor:'#1A237E',
     },
 
